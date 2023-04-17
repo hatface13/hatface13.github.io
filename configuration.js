2bf691febe0f7fb78ba3272131e5d8b04abee142
Hat <hatface13@users.noreply.github.com>
2023-04-17 11:27:00 -0500

    testing a change

---
+++ configuration.js
@@ -0,0 +1,13 @@
+// web location of the content host's dispatch files (dispatch.client.loader.js, DispatchHost.html, etc.)
+DispatchRoot = 'https://mimecast.contentcontroller.com/ScormEngineInterface/dispatch/';
+
+// version of dispatch
+DispatchVersion = 1;
+
+// web location of the content host's launch process which ultimately leads to the content host Engine launch URL
+ContentURL = 'https://mimecast.contentcontroller.com/api/launch/content/3069/3bS-1fxMaznZANkxn6tygB2Txqg8?learnerid=LEARNER_ID&fname=LEARNER_FNAME&lname=LEARNER_LNAME&pipeurl=PIPE_URL&redirecturl=REDIRECT_URL_REGISTRATION_ARGUMENT';
+
+// optional web location that provides pre-launch configuration object
+PreLaunchConfigurationURL = 'https://mimecast.contentcontroller.com/api/launch/config/content/3069/3bS-1fxMaznZANkxn6tygB2Txqg8';
+
+DebugMode=false;
